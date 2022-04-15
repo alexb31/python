6c0d6723943f3cb3d0c9b50046f200281d82ff6e
alexb31 <alexandrebornstein@gmail.com>
2022-04-15 17:31:21 +0200

    importing modules

--- index.py
+++ index.py
@@ -1,7 +1,29 @@
-from email import message
+import os
+import antigravity
+# import datetime
+# import calendar
+# import random
+# from my_module import find_index, test
 
+courses = ['History', 'Math', 'Physics', 'CompSci']
 
-greetings = "Hello";
-name = "Alex";
-message = f"{greetings}, {name}. Welcome"
-print(message);
+print(os.__file__)
+print(antigravity())
+
+# today = datetime.date.today()
+
+# print(today)
+# print(calendar.isleap(2020))
+
+# random_course = random.choice(courses)
+
+# print(random_course)
+
+# index = find_index(courses, "Math")
+# print(index)
+# print(test)
+
+# greetings = "Hello";
+# name = "Alex";
+# message = f"{greetings}, {name}. Welcome"
+# print(message);
