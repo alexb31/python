2ca677d9354d198326e2c3c69a461412c08658fb
alexb31 <alexandrebornstein@gmail.com>
2022-04-21 22:33:05 +0200

    read, update & write csv files

---
+++ csv/parse_csv.py
@@ -0,0 +1,17 @@
+import csv
+
+# with open('names.csv', 'r') as csv_file:
+#     csv_reader = csv.reader(csv_file)
+
+with open('names.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    
+    with open('new_names.csv', 'w') as new_file:
+        field_names = ['first_name', 'last_name', 'email']
+        csv_writer = csv.DictWriter(new_file, fieldnames=field_names, delimiter="\t")
+        
+        csv_writer.writeheader()
+
+        for line in csv_reader:
+            del line['email']
+            csv_writer.writerow(line)
